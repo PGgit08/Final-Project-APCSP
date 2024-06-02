af495beb45a753f4ed9845b8aa416109a56ab18a
Elvis Osmanov <gamerprotime777@gmail.com>
2024-06-02 00:59:34 -0400

    camera

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 import pygame
 from sprites.player import Player
 from sprites.enemy import Enemy
+from camera import Camera
 from globals import *
 
 pygame.init()
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
+bg = pygame.image.load(os.getcwd() + "/assets/background.png")
+bg = pygame.transform.scale(bg, (1000, 1000))
+
+camera = Camera(HEIGHT, WIDTH)
+
 pygame.display.set_caption("Untitled Shooter Game")
 
 dead = False
@@ -17,16 +24,19 @@ def update_sprites():
     enemy_bullets.update()
 
 def draw_sprites():
+    game_display.blit(bg, (0,0))
     players.draw(game_display)
     enemies.draw(game_display)
     player_bullets.draw(game_display)
     enemy_bullets.draw(game_display)
+
 
 ## SETUP CODE
 p = Player()
 e = Enemy()
 e.pos.x = 100
 e.pos.y = 100
+
 
 while not (dead):
     for event in pygame.event.get():
@@ -48,5 +58,6 @@ while not (dead):
 
     update_sprites()
     game_display.fill((0, 0, 0))
+
     draw_sprites()
     pygame.display.flip()
